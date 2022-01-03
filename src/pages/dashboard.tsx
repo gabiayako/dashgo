89e2cfa0a88244cbcc53cf745300d82e8057cb9c
gabiayako <gabiayako@gmail.com>
2022-01-03 14:27:18 -0300

    Adding Sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,16 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 const Dashboard = () => {
-  return <Header />;
+  return (
+    <Flex flexDir="column" h="100vh">
+      <Header />
+      <Flex w="100%" maxW={1200} my="6" px="6" mx="auto">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Dashboard;
