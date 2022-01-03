1df601a77a7733961c9fcf4b34d8e8f49e00bd00
gabiayako <gabiayako@gmail.com>
2022-01-03 15:30:01 -0300

    Adding UserList page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,150 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Text,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilFill } from "react-icons/ri";
+
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+const UserList = () => {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" maxW={1200} my="6" px="6" mx="auto">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar usuário
+            </Button>
+          </Flex>
+
+          <Table colors="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Gabi Ueno</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      gabi@example.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                <Td>04 de Abril 2021</Td>
+
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="green"
+                    leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Gabi Ueno</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      gabi@example.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                <Td>04 de Abril 2021</Td>
+
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="green"
+                    leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Gabi Ueno</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      gabi@example.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                <Td>04 de Abril 2021</Td>
+
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="green"
+                    leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default UserList;
