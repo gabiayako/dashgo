9008157759d79d2ed4ba1b5b0e2247634eb00801
gabiayako <gabiayako@gmail.com>
2022-01-04 18:41:11 -0300

    Adding create user page

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,55 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
+
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+const UserList = () => {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" maxW={1200} my="6" px="6" mx="auto">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
+          <Divider my="6" borderColor="gray.700" />
+          <Stack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="name" label="Nome completo" />
+              <Input name="email" label="E-mail" type="email" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="password" label="Senha" type="password" />
+              <Input
+                name="password_confirmation"
+                label="Confirmação da Senha"
+                type="password"
+              />
+            </SimpleGrid>
+          </Stack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default UserList;
