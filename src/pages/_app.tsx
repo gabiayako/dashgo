61b380f2f2313d50329358c09e41530fea2808b9
gabiayako <gabiayako@gmail.com>
2022-01-24 18:47:01 -0300

    getting user data through react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,20 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { makeServer } from "../services/mirage";
+import { QueryClientProvider, QueryClient } from "react-query";
 
 if (process.env.NODE_ENV === "development") {
   makeServer();
 }
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,14 +12,23 @@ import {
   Th,
   Thead,
   Tr,
+  Spinner,
 } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { RiAddLine, RiPencilFill } from "react-icons/ri";
+import { useQuery } from "react-query";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 const UserList = () => {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+    return data;
+  });
+
   return (
     <Box>
       <Header />
@@ -42,106 +51,114 @@ const UserList = () => {
             </Button>
           </Flex>
 
-          <Table colors="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px="6" color="gray.300" w="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex>Error</Flex>
+          ) : (
+            <Table colors="whiteAlpha">
+              <Thead>
+                <Tr>
+                  <Th px="6" color="gray.300" w="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
 
-                <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th w="8"></Th>
-              </Tr>
-            </Thead>
+                  <Th>Usuário</Th>
+                  <Th>Data de cadastro</Th>
+                  <Th w="8"></Th>
+                </Tr>
+              </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
+              <Tbody>
+                <Tr>
+                  <Td px="6">
+                    <Checkbox colorScheme="pink" />
+                  </Td>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gabi Ueno</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      gabi@example.com
-                    </Text>
-                  </Box>
-                </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">Gabi Ueno</Text>
+                      <Text fontSize="sm" color="gray.300">
+                        gabi@example.com
+                      </Text>
+                    </Box>
+                  </Td>
 
-                <Td>04 de Abril 2021</Td>
+                  <Td>04 de Abril 2021</Td>
 
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="green"
-                    leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="green"
+                      leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td px="6">
+                    <Checkbox colorScheme="pink" />
+                  </Td>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gabi Ueno</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      gabi@example.com
-                    </Text>
-                  </Box>
-                </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">Gabi Ueno</Text>
+                      <Text fontSize="sm" color="gray.300">
+                        gabi@example.com
+                      </Text>
+                    </Box>
+                  </Td>
 
-                <Td>04 de Abril 2021</Td>
+                  <Td>04 de Abril 2021</Td>
 
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="green"
-                    leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="green"
+                      leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td px="6">
+                    <Checkbox colorScheme="pink" />
+                  </Td>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gabi Ueno</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      gabi@example.com
-                    </Text>
-                  </Box>
-                </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">Gabi Ueno</Text>
+                      <Text fontSize="sm" color="gray.300">
+                        gabi@example.com
+                      </Text>
+                    </Box>
+                  </Td>
 
-                <Td>04 de Abril 2021</Td>
+                  <Td>04 de Abril 2021</Td>
 
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="green"
-                    leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="green"
+                      leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          )}
 
           <Pagination />
         </Box>
